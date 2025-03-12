1fa3913891477432a55d38acab99d24aefd3ff7d
Marta-678 <martaroldan972@gmail.com>
2025-03-12 10:42:10 +0100

    comentarios para seguir la practica

--- app.js
+++ app.js
@@ -16,3 +16,14 @@ app.listen(port, () => {
 })
 
 // app.use("/api/users", routersUsers);
+
+
+
+
+
+
+// env_example
+//user pass tiene 3 intentos. 
+//el cliente hace un pos de cliente y pass, coges el usuario del modelo. cifras el pass, generar un clave de 6 ddigitos y guardar en usuario.
+//el servidor manda un email, no es cierto. se pone en consol.log .
+// mandas el usuario con el pass y tiene que comprobar que el usuario corresponde con ese 
