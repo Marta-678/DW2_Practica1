6fb4f45a153665b7818496cad7aba2f759f12c81
Marta-678 <martaroldan972@gmail.com>
2025-03-25 18:41:32 +0100

    añadir registro de usuario con contraseña encriptada
    Crear e implementar regisstro de usuarios, con mathchedData para filtrar datos de entrada.
    
    se cifra la contraseña y compara contraseñas co bcryptjs

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,21 @@
 const UserModel=require('../model/user.js')
+const { matchedData } = require("express-validator");
+const { encrypt, compare } = require("../utils/handlePassword");
+const { usersModel }= require("../model/user.js")
+
+const registerCtrl = async (req, res) => {
+    try{
+        // filtrar los datos para info no válida
+        req=matchedData(req);
+        // cifrear contraseña
+        const pass= await encrypt(req.password);
+        const user = await usersModel.create({ ...req, pass });
+        user.set("password", undefined, { strict: false });
+        
+    }catch (error){
+
+    }
+}
+
+
+module.exports={registerCtrl}
--- router/user.js
+++ router/user.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const UserModel=require('../model/user.js')
+const {validatorRegister}= require("../validators/user.js")
+const {registerCtrl}= require("../controllers/users.js")
 const userRouter = express.Router();
+
+userRouter.post("/register", validatorRegister, registerCtrl);
+
+module.exports= userRouter;
